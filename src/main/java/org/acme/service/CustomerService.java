114a7dd177b3e366e1db40ac33b1b3e27fe3307e
Viktor Falkesjö <viktorfalkesjo@gmail.com>
2024-03-05 10:25:39 +0100

    service

--- src/main/java/org/acme/service/CustomerService.java
+++ src/main/java/org/acme/service/CustomerService.java
@@ -1,5 +1,26 @@
 package org.acme.service;
 
+import org.acme.model.Customer;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+@Transactional(Transactional.TxType.SUPPORTS)
+@ApplicationScoped
+@Named
 public class CustomerService {
+
+    @Inject
+    EntityManager em;
+
+    @Transactional(Transactional.TxType.REQUIRED)
+    public Customer createCustomer(Customer customer) {
+        em.persist(customer);
+        return customer;
+
+    }
 
 }
